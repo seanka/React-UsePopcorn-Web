6b94ba105114d2993bda7e424953f8d9f6fd545f
seanka <and.seanka@gmail.com>
2023-12-17 14:35:55 +0700

    Added cleaning function for data request

--- src/App.js
+++ src/App.js
@@ -73,12 +73,14 @@ export default function App() {
   }
 
   useEffect(() => {
+    const controller = new AbortController();
+
     async function fetchMovies() {
       try {
         setIsLoading(true);
         setError("");
 
-        const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
+        const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`, { signal: controller.signal });
 
         if (!res.ok) throw new Error("No Connection");
 
@@ -89,9 +91,13 @@ export default function App() {
         setError("");
         setIsLoading(false);
         setMovies(data.Search);
+        setError("");
       } catch (err) {
         console.log(err.message);
-        setError(err.message);
+
+        if (err.name !== "AbortError") {
+          setError(err.message);
+        }
       } finally {
         setIsLoading(false);
       }
@@ -105,6 +111,10 @@ export default function App() {
     }
 
     fetchMovies();
+
+    return function () {
+      controller.abort;
+    };
   }, [query]);
 
   return (
